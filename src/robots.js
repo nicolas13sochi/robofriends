09ab031197e41f2b492c6105834d0cc41a1505f1
nicolas13sochi <nikolay.grishchenko@gmail.com>
2019-11-19 01:10:53 +0300

    ready to deploy React app

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Nick Jones',
+        username: 'n13',
+        email: 'n13@test.com'
+    },
+    {
+        id: 2,
+        name: 'Paul Walker',
+        username: 'pw1123',
+        email: 'pw1123@test.com'
+    },
+    {
+        id: 3,
+        name: 'Anna Right',
+        username: 'ar992',
+        email: 'aright992@test.com'
+    },
+    {
+        id: 4,
+        name: 'Joshua Abraham',
+        username: 'ja13xx',
+        email: 'ja13xx@test.com'
+    },
+    {
+        id: 5,
+        name: 'Mike Zinnebad',
+        username: 'mZd',
+        email: 'mZd@test.com'
+    }
+]
