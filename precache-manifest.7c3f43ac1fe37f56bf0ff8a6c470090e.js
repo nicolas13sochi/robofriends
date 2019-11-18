e8a6d487c92db4d255171f0906c52618523bb053
nicolas13sochi <nikolay.grishchenko@gmail.com>
2019-11-19 01:19:31 +0300

    Updates

---
+++ precache-manifest.7c3f43ac1fe37f56bf0ff8a6c470090e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442ec01548e14df539510f5d1b99a3b6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "f7aad51dfc6816c77d2e",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "93569a08e7310dc4c778",
+    "url": "/robofriends/static/css/main.afaa5f78.chunk.css"
+  },
+  {
+    "revision": "f7aad51dfc6816c77d2e",
+    "url": "/robofriends/static/js/2.eebb5bff.chunk.js"
+  },
+  {
+    "revision": "93569a08e7310dc4c778",
+    "url": "/robofriends/static/js/main.09194b32.chunk.js"
+  },
+  {
+    "revision": "cb519fe66fcbb61e7a5c",
+    "url": "/robofriends/static/js/runtime-main.0560b89d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
